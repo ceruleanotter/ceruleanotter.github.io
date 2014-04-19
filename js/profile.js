2a87b66dd2d3fba1aa1e04cbcffc0e487e2784c4
unknown <Lyla@SockMonkeyElite.(none)>
2014-04-19 02:43:44 -0400

    changed resume name, added google analytics

--- js/profile.js
+++ js/profile.js
@@ -7,6 +7,14 @@ $(document).ready(function() {
 	var timer;
 	
 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50120274-1', 'ceruleanotter.github.io');
+  ga('send', 'pageview');
+
 	
 	//little click menu js
 /*	$(document).click(function() {
